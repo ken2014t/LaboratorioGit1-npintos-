6566ab3f287ee3f5d53b32fff8254d716932f085
ken <neoandresp@gmail.com>
2026-05-24 22:55:29 -0500

    compilacion exitosa y nuevo achivo de texto

--- lab.cpp
+++ lab.cpp
@@ -5,13 +5,14 @@ using namespace std;
 
 int main(){
     ifstream entrada("input/entrada.txt");
+    ofstream salida("output/entrada.txt");
 
-    if(entrada.is_open()){
+    if(entrada.is_open() && salida.is_open()){
         cout << "archivo abierto" << endl;
         string linea;
         
         while(getline(entrada, linea)){
-            archivoSalida << linea << endl;
+            salida << linea << endl;
         }
         cout << "Archivo copiado correctamente" << endl;
     }
